f43b726301c2a3979d3dd59785416489ef016775
Himanshu <deep0001berma@gmail.com>
2024-09-18 12:37:30 +0530

    Initial commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>© 2024 Edusity. All rights reserved.</p>
+      <ul>
+        <li>
+          <a href=''>Terms of Services</a>
+        </li>
+        <li>
+          <a>Privacy Policy</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
